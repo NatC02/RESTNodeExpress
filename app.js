bb6235a6fa67f11e8d3dd091ee3e0ec3fdd87e36
NatC02 <NatanCeballos02@gmail.com>
2021-04-15 23:05:24 -0400

    testing GET request and testing POST request on endpoints

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import bodyParser from "body-parser";
 // Set up the express app
 const app = express();
 
-const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,4 +21,50 @@ const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
+});
+
+// Creating another todo through a post request
+
+app.post("/api/v1/todos", (req, res) => {
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: "false",
+      message: "title is required",
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "description is required",
+    });
+  }
+  const todo = {
+    id: db.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+  };
+  db.push(todo);
+  return res.status(201).send({
+    success: "true",
+    message: "todo added successfully",
+    todo,
+  });
+});
+
+//Getting a todo from the database
+
+app.get("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  db.map((todo) => {
+    if (todo.id === id) {
+      return res.status(200).send({
+        success: "true",
+        message: "todo retrieved successfully",
+        todo,
+      });
+    }
+  });
+  return res.status(404).send({
+    success: "false",
+    message: "todo does not exist",
+  });
 });
