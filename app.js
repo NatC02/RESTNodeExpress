30448f5a039dffad39edf2e1629d9e035f9170fb
NatC02 <NatanCeballos02@gmail.com>
2021-04-15 23:21:19 -0400

    testing DELETE and Put Request with previous endpoint, everything seems to working fine

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post("/api/v1/todos", (req, res) => {
   });
 });
 
-//Getting a todo from the database
+//Getting a todo from the database using endpoint below
 
 app.get("/api/v1/todos/:id", (req, res) => {
   const id = parseInt(req.params.id, 10);
@@ -66,5 +66,73 @@ app.get("/api/v1/todos/:id", (req, res) => {
   return res.status(404).send({
     success: "false",
     message: "todo does not exist",
+  });
+});
+
+//Deleting entries in a database using endpoint below
+
+app.delete("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      db.splice(index, 1);
+      return res.status(200).send({
+        success: "true",
+        message: "Todo deleted successfuly",
+      });
+    }
+  });
+
+  return res.status(404).send({
+    success: "false",
+    message: "todo not found",
+  });
+});
+
+//Endpoint that updates todo list
+
+app.put("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if (!todoFound) {
+    return res.status(404).send({
+      success: "false",
+      message: "todo not found",
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: "false",
+      message: "title is required",
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "description is required",
+    });
+  }
+
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title || todoFound.title,
+    description: req.body.description || todoFound.description,
+  };
+
+  db.splice(itemIndex, 1, updatedTodo);
+
+  return res.status(201).send({
+    success: "true",
+    message: "todo added successfully",
+    updatedTodo,
   });
 });
